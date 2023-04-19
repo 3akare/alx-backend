36fc1476accc4ff8384630f0b98fde0791c7f47a
David Bakare <bakaredavid007@gmail.com>
2023-04-19 21:54:21 +0100

    adjusted methods documentations

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-'''BasicCache Class module'''
+'''
+BasicCache Class module
+'''
 
 from base_cache import BaseCaching
 
@@ -8,19 +10,20 @@ class BasicCache(BaseCaching):
     '''
     BasicCache: Inherits from BaseCaching and is a caching system
     '''
-    def __init__(self):
-        '''Inherits all the properties and methods of BaseCaching'''
-        super().__init__()
 
     def put(self, key, item):
-        '''Stores a key-value pair in the cache_data dictionary'''
+        '''
+        Stores a key-value pair in the cache_data dictionary
+        '''
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        '''Retrieves a key-value pair from the cache_data dictionary'''
+        '''
+        Retrieves a value pair from a key in cache_data dictionary
+        '''
         if key is None:
             return None
         return self.cache_data.get(key)
