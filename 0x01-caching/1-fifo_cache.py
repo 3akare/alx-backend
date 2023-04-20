30ba0a6baeb452d52e804be4cbf1740102624822
David Bakare <bakaredavid007@gmail.com>
2023-04-20 01:06:01 +0100

    FIFO cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""FIFOCache Class Module
+"""
+from base_cache import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache: Inherits from BaseCaching and is a
+    caching system
+    """
+    def __init__(self):
+        """
+        init of the Class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Stores a key-value pair in the cache_data dictionary
+        but, discards the first key-value pair when the cache is full.
+
+        The cache has a size of 4
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_item = list(self.cache_data.keys())[0]
+                del self.cache_data[first_item]
+                print(f'DISCARD: {first_item}')
+
+    def get(self, key):
+        """Returns the value linked to a key from the cache
+        """
+        return self.cache_data.get(key)
