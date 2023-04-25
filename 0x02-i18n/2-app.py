728d808836168159fada257b5b62094993c0c744
David Bakare <bakaredavid007@gmail.com>
2023-04-25 23:20:59 +0100

    get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+Config Class
+'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    '''
+    Config Class: Used along side with the 'app.config.from.object()
+    function provided by babel to config it'
+    '''
+    LANGUAGES: list = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    Selects a langauage translation to use for the request
+    '''
+    lang = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return lang or app.config['BABEL_DEFAULT_LOCALE']
+
+
+@app.route('/')
+def index():
+    '''
+    Returns the 2-index.html from the templates
+    directory
+    '''
+    return render_template('/2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
