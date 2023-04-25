85e604e7b0ccf4fa5c59894e2cc7ea57eba0b1e6
David Bakare <bakaredavid007@gmail.com>
2023-04-25 23:04:18 +0100

    added route function

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,9 +3,8 @@
 Config Class
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
-from typing import List
 
 app = Flask(__name__)
 
@@ -15,10 +14,23 @@ class Config:
     Config Class: Used along side with the 'app.config.from.object()
     function provided by babel to config it'
     '''
-    LANGUAGES: List[str, str] = ['en', 'fr']
+    LANGUAGES: list = ['en', 'fr']
     BABEL_DEFAULT_LOCALE: str = 'en'
     BABEL_DEFAULT_TIMEZONE: str = 'UTC'
 
 
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    '''
+    Returns the 1-index.html from the templates
+    directory
+    '''
+    return render_template('/1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
