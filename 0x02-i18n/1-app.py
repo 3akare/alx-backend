49361f615035f5c750d1b452f7a4be80020c4ca0
David Bakare <bakaredavid007@gmail.com>
2023-04-25 22:57:40 +0100

    config class

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Config Class
+'''
+
+from flask import Flask
+from flask_babel import Babel
+from typing import List
+
+app = Flask(__name__)
+
+
+class Config:
+    '''
+    Config Class: Used along side with the 'app.config.from.object()
+    function provided by babel to config it'
+    '''
+    LANGUAGES: List[str, str] = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE: str = 'en'
+    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
